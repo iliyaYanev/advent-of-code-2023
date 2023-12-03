6129c0521eb78dd96c387455eeb4047b5f785a8f
Iliya Yanev <iliyayanev92@gmail.com>
2023-12-03 10:42:28 +0200

    use point record

--- src/main/java/util/Point.java
+++ src/main/java/util/Point.java
@@ -2,17 +2,10 @@ package util;
 
 import java.util.List;
 
-public class Point {
-    public int x;
-    public int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+public record Point(int x, int y) {
 
     public Point add(Point delta) {
-        return new Point(x + delta.x, y + delta.y);
+        return new Point(x + delta.x(), y + delta.y());
     }
 
     public List<Point> surroundingPoints() {
@@ -26,22 +19,5 @@ public class Point {
             new Point(x, y + 1),
             new Point(x + 1, y + 1)
         );
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return x == point.x && y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return 31 * x + y;
-    }
-
-    public String toString() {
-        return getClass().getName() + "[x=" + x + ",y=" + y + "]";
     }
 }
