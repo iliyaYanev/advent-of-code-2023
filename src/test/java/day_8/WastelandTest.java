f3bb8e0a800aac6a2a86fe09f80ff5f90467a648
Iliya Yanev <iliya.yanev@egt-digital.com>
2023-12-08 10:37:20 +0200

    refactor day 8 tests

--- src/test/java/day_8/WastelandTest.java
+++ src/test/java/day_8/WastelandTest.java
@@ -2,29 +2,36 @@ package day_8;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import util.GetInputFileContents;
 
 public class WastelandTest {
 
+    private static final String FILE_CONTENTS;
+
+    static {
+        try {
+            FILE_CONTENTS = GetInputFileContents.getFileAsString("src/test/resources/day_8/dayEightInput.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @Test
-    public void wastelandPartOneTest() throws IOException {
-        String fileContents = GetInputFileContents
-            .getFileAsString("src/test/resources/day_8/dayEightTestInputPartOne.txt");
+    public void wastelandPartOneTest() {
+        long result = Wasteland.wastelandStepsPartOne(FILE_CONTENTS);
 
-        long result = Wasteland.wastelandStepsPartOne(fileContents);
-
-        assertEquals(2, result);
+        assertEquals(19631, result);
     }
 
     @Test
-    public void wastelandPartTwoTest() throws IOException {
-        String fileContents = GetInputFileContents
-            .getFileAsString("src/test/resources/day_8/dayEightTestInputPartTwo.txt");
+    public void wastelandPartTwoTest() {
+        long result = Wasteland.wastelandStepsPartTwo(FILE_CONTENTS);
 
-        long result = Wasteland.wastelandStepsPartTwo(fileContents);
-
-        assertEquals(6, result);
+        assertEquals(21003205388413L, result);
     }
 }
